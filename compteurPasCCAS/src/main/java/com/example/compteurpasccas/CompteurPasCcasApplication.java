8652361ab0723494246c30367c585cfc9414d3d4
Biinbin <gabin.lasservarie@etud.univ-jfc.fr>
2023-01-09 13:48:19 +0100

    Debut

---
+++ compteurPasCCAS/src/main/java/com/example/compteurpasccas/CompteurPasCcasApplication.java
@@ -0,0 +1,13 @@
+package com.example.compteurpasccas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompteurPasCcasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompteurPasCcasApplication.class, args);
+    }
+
+}
