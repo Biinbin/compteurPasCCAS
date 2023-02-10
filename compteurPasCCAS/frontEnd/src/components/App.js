e448fee4dd563ca01e10ec8b243bbae18793f4e5
FlorentBonnafous <florentbonnafousts1@gmail.com>
2023-02-10 12:52:19 +0100

    début frontEnd

---
+++ compteurPasCCAS/frontEnd/src/components/App.js
@@ -0,0 +1,16 @@
+import '../style/App.css';
+import Banner from './Banner'
+import Compteur from './Compteur';
+import Villes from './Villes'
+
+function App() {
+	return (
+		<div>
+			<Banner />
+			<Compteur />
+			<Villes />
+		</div>
+	)
+}
+
+export default App
---
+++ compteurPasCCAS/frontEnd/src/components/Compteur.js
@@ -0,0 +1,13 @@
+import '../style/Compteur.css'
+
+function Compteur() {
+    
+    return (
+      <form id='addPas'>
+        <input type="number" />
+        <button type="submit">Ajouter vos pas</button>
+      </form>
+    );
+}
+
+export default Compteur
