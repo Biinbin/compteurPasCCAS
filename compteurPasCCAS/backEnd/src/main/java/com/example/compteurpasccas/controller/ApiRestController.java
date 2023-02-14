d67b91d95aa10370c7f3c87006b1345fc0736011
Arnaud <79163961+BonnafJr@users.noreply.github.com>
2023-02-14 20:29:02 +0100

    affichage compteur et ajout de pas

--- compteurPasCCAS/backEnd/src/main/java/com/example/compteurpasccas/controller/ApiRestController.java
+++ compteurPasCCAS/backEnd/src/main/java/com/example/compteurpasccas/controller/ApiRestController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("api")
 public class ApiRestController {
