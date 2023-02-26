d0a490a83b107ba8b6f5666ce8714f46c3a89bf0
BonnafJr <bonnafousarnaud@gmail.com>
2023-02-26 20:17:39 +0100

    ajout affichage villes et compteur

--- compteurPasCCAS/frontEnd/src/components/StepCounter.js
+++ compteurPasCCAS/frontEnd/src/components/StepCounter.js
@@ -2,19 +2,23 @@ import '../style/StepCounter.css'
 import React, { useState, useEffect } from 'react';
 
 const StepCounter = () => {
-  const [stepCount, setStepCount] = useState(0);
-  const [unit, setUnit] = useState('pas');
-  const [valueToAdd, setValueToAdd] = useState(0);
+    const [stepCount, setStepCount] = useState(0);
+    const [unit, setUnit] = useState('pas');
+    const [valueToAdd, setValueToAdd] = useState(0);
 
-  useEffect(() => {
-    // Récupère la valeur actuelle du compteur de pas à partir de l'API REST
-    fetch('http://localhost:8080/api/counter')
-      .then(response => response.json())
-      .then(data => setStepCount(data))
-      .catch(error => console.error(error));
-  }, []);
+    useEffect(() => {
+        // Récupère la valeur actuelle du compteur de pas à partir de l'API REST
+        fetch('http://localhost:8080/api/counter')
+            .then(response => response.json())
+            .then(data => setStepCount(data.valeur))
+            .catch(error => console.error(error));
+    }, []);
 
     const handleSave = () => {
+        if (stepCount === undefined) {
+            console.log('Erreur: stepCount est indéfini');
+            return;
+        }
         let url = 'http://localhost:8080/api/counter/steps';
         if (unit === 'kilomètre') {
             url = 'http://localhost:8080/api/counter/km';
@@ -23,27 +27,30 @@ const StepCounter = () => {
         console.log('Valeur actuelle de stepCount :', stepCount);
         console.log('Unité : ', unit);
         console.log('url : ', url);
+
+        const counterToUpdate = { value: valueToAdd };
         // Enregistre les modifications apportées au compteur de pas ou de km dans l'API REST
         fetch(url, {
             method: 'PUT',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ stepCount: stepCount + valueToAdd})
+            body: JSON.stringify(counterToUpdate)
         })
-        .then(response => response.json())
-            .then(data => setStepCount(data))
+            .then(response => response.json())
+            .then(data => setStepCount(data.valeur))
+            .catch(error => console.error(error));
     };
 
-  return (
-    <div>
-      <p>Nombre de kilomètre: {stepCount}</p>
-      <input type="number" value={valueToAdd} onChange={e => setValueToAdd(e.target.value)} />      
-      <select value={unit} onChange={e => setUnit(e.target.value)}>
-        <option value="pas">Pas</option>
-        <option value="kilomètre">Kilomètre</option>
-      </select>
-      <button onClick={handleSave}>Enregistrer</button>
-    </div>
-  );
+    return (
+        <div>
+            <p>Nombre de kilomètres : {stepCount}</p>
+            <input type="number" value={valueToAdd} onChange={e => setValueToAdd(e.target.value)} />
+            <select value={unit} onChange={e => setUnit(e.target.value)}>
+                <option value="pas">Pas</option>
+                <option value="kilomètre">Kilomètre</option>
+            </select>
+            <button onClick={handleSave}>Enregistrer</button>
+        </div>
+    );
 };
 
 export default StepCounter;
--- compteurPasCCAS/frontEnd/src/components/Villes.js
+++ compteurPasCCAS/frontEnd/src/components/Villes.js
@@ -1,21 +1,79 @@
+import { useEffect, useState } from 'react';
 import '../style/Villes.css'
 
-function Villes() {
-	const parisKm = 8
-	const londresKm = 10
-	const athenesKM = 15
-	
+function Villes({ distance }) {
+	const [villes, setVilles] = useState([]);
+
+	useEffect(() => {
+		// Récupère la liste des villes à partir de l'API REST
+		fetch('http://localhost:8080/api/city')
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data.cities);
+				setVilles(data.cities);
+			});
+	}, []);
+
+	const [StepCount, setStepCount] = useState(0);
+
+	useEffect(() => {
+		// Récupère la valeur actuelle du compteur de pas à partir de l'API REST
+		fetch('http://localhost:8080/api/counter')
+			.then(response => response.json())
+			.then(data => setStepCount(data.valeur))
+			.catch(error => console.error(error));
+	}, []);
+
+	const [Distance, setDistance] = useState(0);
+
+	useEffect(() => {
+		// Récupère la distance des villes à partir de l'API REST
+		fetch('http://localhost:8080/api/counter/distance/${id}')
+			.then(response => response.json())
+			.then(data => setDistance(data.valeur))
+			.catch(error => console.error(error));
+	}, []);
+
+	const [infosVille, setInfosVille] = useState('');
+
+	const afficherInfos = (id) => {
+		// Affiche les informations des villes
+		if (estVerrouillee(id)) {
+			//Réécupère les informations des villes
+			fetch(`http://localhost:8080/api/city/info/${id}`)
+				.then((response) => response.text())
+				.then((data) => { setInfosVille(data);
+				});
+		}
+	};
+
+	const estVerrouillee = (id) => {
+		const ville = villes.find(v => v.id === id);
+		return ville >= ville.distance;
+	};
+
 	return (
-		<div className='lmj-villes'>
-			<h2>Les villes hôtes</h2>
-			<ul>
-				<li>Paris : {parisKm}km</li>
-				<li>Londres : {londresKm}km</li>
-				<li>Athènes : {athenesKM}km</li>
-			</ul>
-			Total : {parisKm + londresKm + athenesKM}km
+		<div className="villes-list">
+			{villes &&
+				villes.map((ville) => (
+					<div className="ville" key={ville.id}>
+						{estVerrouillee(ville.id) && (
+							<div className="infos-ville">{infosVille}</div>
+						)}
+						<img
+							src={ville.urlImg}
+							alt={ville.nom}
+							onError={(e) => {
+								e.target.onerror = null;
+								e.target.src = '/default-image.jpg';
+							}}
+						/>
+						<h2>{ville.nom}</h2>
+						<button onClick={() => afficherInfos(ville.id)}>Informations</button>
+					</div>
+				))}
 		</div>
-	)
+	);
 }
 
-export default Villes
+export default Villes;
